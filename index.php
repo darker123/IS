66c58c54d0a706c3a9db1b6462aa7ffd16a65895
Ariel <arigato20amn@gmial.com>
2017-09-24 10:35:50 -0400

    tabla terminada

--- index.php
+++ index.php
@@ -9,8 +9,16 @@
 	$a = 'hola mundo desde php';
 	echo $a;
 ?>
-<table>
-	<li>nro</li>
+<table style="width:50%">
+  <tr>
+    <th>Name</th>
+    <th colspan="1">Telephone</th>
+  </tr>
+  <tr>
+    <td>Bill Gates</td>
+    <td>55577854</td>
+    <td>55577855</td>
+  </tr>
 </table>
 </body>
 </html>
